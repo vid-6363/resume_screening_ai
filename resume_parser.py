1f952ac86a2ac0091bf64585589090c08829729e
vidyalaxmi <137512758+vid-6363@users.noreply.github.com>
2025-12-19 15:09:37 +0530

    Create resume_parser.py

---
+++ resume_parser.py
@@ -0,0 +1,8 @@
+import PyPDF2
+
+def extract_text(pdf_file):
+    reader = PyPDF2.PdfReader(pdf_file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
